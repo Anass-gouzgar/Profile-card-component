5e453cf3fb3ac0c7884eb4a73f1a5e0d125c1090
anass-gouzgar <go.anass.gouzgar@gmail.com>
2024-05-22 01:06:41 +0100

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,6 @@ import Card from "./components/Card"
 export default function Home() {
   return (
     <main className="bg-Dark-cyan flex justify-center items-center h-screen w-screen bgCombined overflow-hidden ">
-      {/* <div className="topBG overflow-hidden " /> */}
-      {/* <div className="bottomBG overflow-hidden " /> */}
       <Card />
     </main>
   );
